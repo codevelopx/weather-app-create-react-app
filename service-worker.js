af1dd8482dbf122d3d1a32e3f1db6cb5dfe4556b
codevelop <codevelopx@gmail.com>
2019-06-04 15:02:48 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c5ca818d79bd727b5b6864d00e1ff50.js"
+  "/precache-manifest.807c864712b08b372aaf452affdd7f31.js"
 );
 
 self.addEventListener('message', (event) => {
