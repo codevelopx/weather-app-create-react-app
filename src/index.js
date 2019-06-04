6ecc8ac6e4bae62c1db1ae596710ba0547cb411c
codevelop <codevelopx@gmail.com>
2019-06-04 15:07:56 +0200

    Add deployment https://codevelopx.github.io/weather-app-create-react-app/

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
 
-    <BrowserRouter basename='/weather-app-create-react-appgi'>
+    <BrowserRouter basename='/weather-app-create-react-app'>
         <App />
     </BrowserRouter >,
     document.getElementById('root'));
