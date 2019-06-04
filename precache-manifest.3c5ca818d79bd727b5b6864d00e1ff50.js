b17947e29efc98de94a9df7b81d9ceebe1cc5960
codevelop <codevelopx@gmail.com>
2019-06-04 14:44:23 +0200

    Updates

--- precache-manifest.3c5ca818d79bd727b5b6864d00e1ff50.js
+++ precache-manifest.3c5ca818d79bd727b5b6864d00e1ff50.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4b5d599542f6c2ffa3fc90e99e3b4a46",
+    "revision": "6e412b5dfaebb4924e6465e4ab322db8",
     "url": "/index.html"
   },
   {
-    "revision": "6db77a4c4715d97d886b",
+    "revision": "d22d076cee1db400ebbd",
     "url": "/static/css/main.17744510.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.613d9787.chunk.js"
   },
   {
-    "revision": "6db77a4c4715d97d886b",
-    "url": "/static/js/main.571ae8a6.chunk.js"
+    "revision": "d22d076cee1db400ebbd",
+    "url": "/static/js/main.55959d9e.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
