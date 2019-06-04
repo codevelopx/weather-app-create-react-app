9be6214ff9698d034a73a6e4f4f100725b4e3b90
codevelop <codevelopx@gmail.com>
2019-06-04 14:31:49 +0200

    Updates

---
+++ precache-manifest.fbc960ca5c9b7151419d20a7c178ddb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5d599542f6c2ffa3fc90e99e3b4a46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6db77a4c4715d97d886b",
+    "url": "/static/css/main.17744510.chunk.css"
+  },
+  {
+    "revision": "3a53f407dacba6b7e43b",
+    "url": "/static/js/2.613d9787.chunk.js"
+  },
+  {
+    "revision": "6db77a4c4715d97d886b",
+    "url": "/static/js/main.571ae8a6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
